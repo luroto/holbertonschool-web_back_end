72597943a6f511cb106dc57ee485a4eaa18d2a39
Lucia Rodriguez Toloza <774@holbertonschool.com>
2021-03-16 02:47:41 +0000

    [Feature] Fixing variable annotation

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -3,7 +3,7 @@
 Defining and annotating variables
 """
 
-a = int(1)
-pi = float(3.14)
-i_understand_annotations = True
-school = str("Holberton")
+a : int = 1
+pi : float = 3.14
+i_understand_annotations : bool = True
+school : str = "Holberton"
