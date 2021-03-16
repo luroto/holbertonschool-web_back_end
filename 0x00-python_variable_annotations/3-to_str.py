9f82f79e9999f56c66e316f141a7e133a2f6cceb
Lucia Rodriguez Toloza <774@holbertonschool.com>
2021-03-16 01:30:05 +0000

    [Fix] trailing whitespace

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -6,6 +6,6 @@ Working with to_string methods
 
 def to_str(n: float) -> str:
     """
-    Function to convert Float input to string 
+    Function to convert Float input to string
     """
     return str(n)
