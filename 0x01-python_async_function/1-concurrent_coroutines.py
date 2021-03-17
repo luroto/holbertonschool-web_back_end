c414230294b558752e82b0ac73f9373c14404a06
Lucia Rodriguez Toloza <774@holbertonschool.com>
2021-03-17 03:02:49 +0000

    [Feature] Uploading files for task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,22 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Multiple coroutines
+"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """
+    Executing multiple coroutines given a first function
+    """
+    all_tasks = []
+    all_delays = []
+    delay = 0
+    for _ in range(n):
+        all_tasks.append(asyncio.create_task(wait_random(max_delay)))
+
+    for task in all_tasks:
+        all_delays.append(await(task))
+    return all_delays
