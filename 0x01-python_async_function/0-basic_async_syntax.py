142a1ab4ca57371808e6c69547e5c43c412c2251
Lucia Rodriguez Toloza <774@holbertonschool.com>
2021-03-17 02:10:06 +0000

    [Feature] Uploading file for task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,11 +3,14 @@
 First asyncio task
 """
 import asyncio
+import random
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int=10) -> float:
     """
     Asynchronous coroutine that takes in an integer argument that
     waits for a random delay between 0 and max_delay returning it
     """
-    await asyncio.sleep(max_delay)
-    return 
+
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
