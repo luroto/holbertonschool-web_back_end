8515e0af34662aede8c71a6e44fc10e83197d4a4
Lucia Rodriguez Toloza <774@holbertonschool.com>
2021-03-17 03:22:27 +0000

    [Feature] Uploading file for second time

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,1 +1,20 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Measuring runtime
+"""
+
+import asyncio
+import time
+from typing import List
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int = 10) -> float:
+    """
+    Function for measuring execution time of concurrent tasks
+    """
+    t0 = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    t1 = time.time()
+    fulltime = t1 - t0
+    return fulltime / n
