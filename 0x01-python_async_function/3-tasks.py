57c657d4c0e50e7145186a121e98806d29a04e94
Lucia Rodriguez Toloza <774@holbertonschool.com>
2021-03-17 03:33:41 +0000

    [Feature] Uploading file for third task

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,1 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Creating tasks
+"""
+
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int = 10) -> asyncio.Task:
+    """
+    Returns a Task object
+    """
+    running = asyncio.get_event_loop()
+    return running.create_task(wait_random(max_delay))
