a15a7609fc4674a7037c3f7cdd26d612c50bf75b
Lucia Rodriguez Toloza <774@holbertonschool.com>
2021-03-23 01:31:22 +0000

    [Feature]Uploading files for 4th task

---
+++ 0x03-caching/4-mru_cache.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+Implementing the MRU caching police
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    Class for handling MRU caching system
+    """
+    def __init__(self):
+        """
+        Constructor for the class
+        """
+        super().__init__()
+        self.recent_keys = []
+
+    def put(self, key, item):
+        """
+            Method for adding key and value to the dict
+        """
+        self.cache_data[key] = item
+        if key not in self.recent_keys:
+            self.recent_keys.append(key)
+        else:
+            if key != self.recent_keys[-1]:
+                self.recent_keys.remove(key)
+                self.recent_keys.append(key)
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            recent = self.recent_keys[-2]
+            del self.cache_data[recent]
+            self.recent_keys.remove(recent)
+            print("Discard: {}".format(recent))
+
+    def get(self, key):
+        """
+        Method for getting a value from dict and tracks its use
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        if key != self.recent_keys:
+            self.recent_keys.remove(key)
+            self.recent_keys.append(key)
+        return self.cache_data[key]
