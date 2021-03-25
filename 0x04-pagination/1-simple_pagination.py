8a87d1e3c2d44dde5c137f706d741a30434f7b0c
Lucia Rodriguez Toloza <774@holbertonschool.com>
2021-03-25 01:36:33 +0000

    [Feature] Uploading files for the first task

---
+++ 0x04-pagination/1-simple_pagination.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Function for getting the page content
+        """
+        assert isinstance(page, int) is True
+        assert isinstance(page_size, int) is True
+        assert page > 0
+        assert page_size > 0
+
+        looking = Server.index_range(page, page_size)
+        returning = []
+        with open('Popular_Baby_Names.csv', 'r') as openingFile:
+            readingFile = csv.reader(openingFile, delimiter=',')
+            i = 0
+            for line in readingFile:
+                if i == 0:
+                    pass
+                elif looking[0] <= i <= looking[1]:
+                    returning.append(line)
+                i += 1
+            return returning
+
+    def index_range(page: int, page_size: int) -> tuple:
+        """
+        Calculates the indexes contained in a page given a page_size
+        """
+        return ((page - 1) * page_size, page * page_size)
