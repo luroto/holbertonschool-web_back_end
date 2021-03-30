ed1ab916a9aaa4a3eb80c7bf688f7d8a9db135db
Lucia Rodriguez Toloza <774@holbertonschool.com>
2021-03-30 02:59:20 +0000

    [Fix] Fixing pycodestyle

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -6,10 +6,12 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction:str, message:str, separator:str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """
-    Function that replaces sensible data based on regex 
+    Function that replaces sensible data based on regex
     """
     for field in fields:
-        message = re.sub(r"{}=.*?{}".format(field, separator), "{}={}{}".format(field, redaction, separator), message) 
+        message = re.sub(r"{}=.*?{}".format(field, separator), "{}={}{}"
+                         .format(field, redaction, separator), message)
     return message
