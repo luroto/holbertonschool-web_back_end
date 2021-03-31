a83b44fc8392bfd3b98bfe985922d4ed53e6b884
Lucia Rodriguez Toloza <774@holbertonschool.com>
2021-03-31 03:13:39 +0000

    [Feature] Uploading for the second task

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -2,8 +2,32 @@
 """
 Filtered logger powered by regex
 """
+import logging
 import re
 from typing import List
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Function to format a log
+        """
+        '''Instead of doing a record['msg'], we use the mother class format
+        method to generate the message and then apply the remaining
+        parameters for filter datum '''
+        return(filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR))
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
