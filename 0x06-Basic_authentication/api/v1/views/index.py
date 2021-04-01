1cbe7bb50a7c614fa478e6b8ffb1a5435bbc00c9
Lucia Rodriguez Toloza <774@holbertonschool.com>
2021-04-01 04:49:05 +0000

    [Feature] Adding some tasks

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,20 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """GET /api/v1/unauthorized
+    Return:
+    Unauthorized access
+    """
+    abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """
+    Forbidden access via GET/api/v1/forbidden
+    """
+    abort(403)
