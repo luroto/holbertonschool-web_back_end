3cb542ecac87e77c4caa921022be5af34cdbbbed
Lucia Rodriguez Toloza <774@holbertonschool.com>
2021-04-03 23:53:10 +0000

    [Feature] Uploading checker for paths

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -14,7 +14,14 @@ class Auth():
         """
         Public methor for require auth check
         """
-        return False
+        if path is None:
+            return True
+        if path not in excluded_paths:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path in excluded_paths:
+            return False
 
     def authorization_header(self, request=None) -> str:
         """
