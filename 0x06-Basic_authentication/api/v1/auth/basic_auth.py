7b8b96d43b22ec1f6805766351c369801633ccf0
Lucia Rodriguez Toloza <774@holbertonschool.com>
2021-04-06 01:50:25 +0000

    [Feature] Uploading the decode method for BasicAuth class

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,7 +5,7 @@ Defining BasicAuth Classes
 from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
-
+import base64
 
 class BasicAuth(Auth):
     """
@@ -24,3 +24,18 @@ class BasicAuth(Auth):
         if authorization_header[:6] != "Basic ":
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:
+        """
+        Returns the decoded value of a Base64 string
+        """
+        if base64_authorization_header is None:
+            return None
+        if isinstance(base64_authorization_header, str) is not True:
+            return None
+        try:
+            decod = base64.b64decode(base64_authorization_header.encode('utf-8'))
+            return decod.decode('utf-8')
+        except Exception:
+            return None
