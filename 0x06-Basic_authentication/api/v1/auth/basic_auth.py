fa3decf683ebe7ec65001aaf8a2a407312ad4f1a
Lucia Rodriguez Toloza <774@holbertonschool.com>
2021-04-06 01:53:48 +0000

    [Fix] Fixing pycodestyle

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,6 +7,7 @@ from typing import List, TypeVar
 from api.v1.auth.auth import Auth
 import base64
 
+
 class BasicAuth(Auth):
     """
     Based on Auth we create the Basic Auth
@@ -26,7 +27,8 @@ class BasicAuth(Auth):
         return authorization_header[6:]
 
     def decode_base64_authorization_header(self,
-                                           base64_authorization_header: str) -> str:
+                                           base64_authorization_header: str
+                                           ) -> str:
         """
         Returns the decoded value of a Base64 string
         """
@@ -35,7 +37,8 @@ class BasicAuth(Auth):
         if isinstance(base64_authorization_header, str) is not True:
             return None
         try:
-            decod = base64.b64decode(base64_authorization_header.encode('utf-8'))
+            decod = base64.b64decode(
+                    base64_authorization_header.encode('utf-8'))
             return decod.decode('utf-8')
         except Exception:
             return None
