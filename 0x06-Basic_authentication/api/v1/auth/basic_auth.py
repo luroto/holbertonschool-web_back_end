6ada25044817b0e4b2e1e7abe9db56eb1051b7b3
Lucia Rodriguez Toloza <774@holbertonschool.com>
2021-04-06 00:49:54 +0000

    [Feature] Uploading basic auth

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,3 +11,16 @@ class BasicAuth(Auth):
     """
     Based on Auth we create the Basic Auth
     """
+    def extract_base64_authorization_header(self, authorization_header: str)
+    -> str:
+        """
+        Returns the Base64 part of the Authorization header for a basic
+        Authentication
+        """
+        if authorization_header is None:
+            return None
+        if isinstance(authorization_header, str) is not True:
+            return None
+        if authorization_header[:6] != "Basic ":
+            return None
+        return authorization_header[6:]
