005c2e39aa70c2ccd2d9a69084e2fd3774904b7b
Lucia Rodriguez Toloza <774@holbertonschool.com>
2021-04-07 13:08:27 +0000

    [Feature] Adding user & password

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -42,3 +42,16 @@ class BasicAuth(Auth):
             return decod.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        returns the user email and password from the Base64 decoded value
+        """
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        if ":" not in decoded_base64_authorization_header:
+            return (None, None)
+        contents = decoded_base64_authorization_header.split(":")
+        return (contents[0], [1])
