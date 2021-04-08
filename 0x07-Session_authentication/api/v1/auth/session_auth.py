28a692b72c4fa3aea07d7c1bb0e7f46751d42c2f
Lucia Rodriguez Toloza <774@holbertonschool.com>
2021-04-08 02:20:40 +0000

    [Feature] Uploading SessionAuth

---
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Defining SessionAuth Classes
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    Class for SessionAuth inheriting from Auth()
+    """
