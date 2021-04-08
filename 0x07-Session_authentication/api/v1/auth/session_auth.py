802efd4ad359ca55972eeafd7357676134d3b706
Lucia Rodriguez Toloza <774@holbertonschool.com>
2021-04-08 02:56:16 +0000

    [Feature] Uploading user and session method

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -21,6 +21,16 @@ class SessionAuth(Auth):
             return None
         if isinstance(user_id, str) is not True:
             return None
-        session_id = uuid.uuid4()
+        session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Returns User ID based on Session ID
+        """
+        if session_id is None:
+            return None
+        if isinstance(session_id, str) is not True:
+            return None
+        return self.user_id_by_session_id.get(session_id)
