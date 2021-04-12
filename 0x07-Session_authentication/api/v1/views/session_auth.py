a4a89824d5f7f62284db9baf976ccf01c936ff5f
Lucia Rodriguez Toloza <774@holbertonschool.com>
2021-04-12 00:29:34 +0000

    [Feature] Adding files for new view

---
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Module of session authviews
+"""
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models.user import User
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def all_routes() -> str:
+    """ POST /api/v1/auth_session/login
+    for handling session_auth routes
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email is None:
+        return jsonify({"error": "email missing"}), 400
+    if not password or len(password) == 0:
+        return jsonify({"error": "password missing"}), 400
+    usuario = User()
+    busqueda = usuario.search({'email': email})
+    if len(busqueda) == 0:
+        return jsonify({"error": "no user found for this email"}), 404
+    if busqueda[0].is_valid_password(password) is False:
+        return jsonify({"error": "wrong password"}), 401
+    from api.v1.app import auth
+    return busqueda[0].to_json()
