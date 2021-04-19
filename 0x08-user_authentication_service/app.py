c8c9fae062dc4070486916796cf8cb32233ca6e6
Lucia Rodriguez Toloza <774@holbertonschool.com>
2021-04-19 03:12:49 +0000

    [Feature] Uploading POST route

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -5,9 +5,10 @@ Route module for the API
 from os import getenv
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-
+from auth import Auth
 import os
 
+AUTH = Auth()
 
 app = Flask(__name__)
 
@@ -18,6 +19,25 @@ def firstRoute():
     First route to be implemented
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users():
+    """
+    Route for creating users via Auth
+    """
+    data = request.get_data()
+    data = data.decode('utf-8')
+    allData = {}
+    allItems = data.split('&')
+    for item in allItems:
+        pair = item.split('=')
+        allData[pair[0]] = pair[1]
+    try:
+        AUTH.register_user(allData['email'], allData['password'])
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": allData['email'], "message": "user created"}), 400
 
 
 if __name__ == "__main__":
