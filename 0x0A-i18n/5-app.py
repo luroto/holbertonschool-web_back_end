e9158616df2f74a7479c22b15f728ac3f05834b3
Lucia Rodriguez Toloza <774@holbertonschool.com>
2021-04-26 01:27:54 +0000

    [Fix] Fixing third task

--- 0x0A-i18n/5-app.py
+++ 0x0A-i18n/5-app.py
@@ -6,6 +6,14 @@ from flask import Flask, request, render_template, g
 from flask_babel import Babel, gettext
 
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
 class Config(object):
     """
     Configuration class for Babel
